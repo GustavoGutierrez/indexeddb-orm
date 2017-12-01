22624b43454dd0a2922a6b9f41d539df448110af
Max Gaurav <max.gaurav@ithands.net>
2017-12-01 17:15:44 +0530

    add test case: checking existance of idb function and return instacne type of function call

--- test/cases.js
+++ test/cases.js
@@ -1,12 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-describe('checking existence of idb', function () {
+describe('cases for idb function existence', function () {
     it('should exist', function () {
         expect(typeof window.idb).toBe('function');
     });
     it('function call should return instance of DB', function () {
         var idb = window.idb({});
-        console.log(typeof idb, idb);
         expect(idb instanceof window.DB).toBe(true);
     });
+});
+describe('cases for idb connection actions', function () {
+    window.indexedDB.deleteDatabase('test');
+    var idb = window.idb({});
 });
--- test/cases.ts
+++ test/cases.ts
@@ -1,14 +1,19 @@
 import {Settings} from "../src/interfaces";
 
-describe('checking existence of idb', () => {
+describe('cases for idb function existence', () => {
     it('should exist', () => {
         expect(typeof window.idb).toBe('function');
     });
 
     it('function call should return instance of DB', () => {
         let idb = window.idb(<Settings>{});
-        console.log(typeof idb, idb);
 
         expect(idb instanceof window.DB).toBe(true);
     });
+});
+
+
+describe('cases for idb connection actions', () => {
+    window.indexedDB.deleteDatabase('test');
+    let idb = window.idb({});
 });
